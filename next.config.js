8035c56281b63fcbf341fb11614086b52504d4d8
AgenciaSpline <jonatasrodrigues.n8n@gmail.com>
2026-09-05 03:48:54 +0000

    chore: limpeza geral de scripts temporarios e logs do repositorio

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-// Force restart: 2026-08-31
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
--- run-prisma.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /root/projeto/crm-eleitoral
-npx prisma db push 2>&1
-npx prisma generate 2>&1
